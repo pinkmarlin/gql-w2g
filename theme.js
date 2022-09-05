c2ed0a3de787332fed42ce88fa4b35a5fed85a66
Kenny Hammerlund <kenny.hammerlund@marriott.com>
2022-09-04 19:41:46 -0500

    chore: setup

---
+++ theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    ImperialRed: "e63946",
+    Honeydew: "f1faee",
+    PowderBlue: "a8dadc",
+    CeladonBlue: "457b9d",
+    PrussianBlue: "1d3557",
+  },
+};
